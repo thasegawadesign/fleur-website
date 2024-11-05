294a485191627f1324de723b90a9266730e66b04
thasegawadesign <hasegawatatsuya206@gmail.com>
2024-11-05 15:03:58 +0900

    add: breakpoints

---
+++ src/styles/styles.css.ts
@@ -0,0 +1,7 @@
+export const breakpoints = {
+  sm: "screen and (width < 640px)",
+  md: "screen and (640px <= width < 768px)",
+  lg: "screen and (768px <= width < 1024px)",
+  xl: "screen and (1024px <= width < 1280px)",
+  "2xl": "screen and (1280px <= width)",
+} as const;
